ae85e58a9717be1faad35adaa3151e4fbb64d40d
umutxcan <canumut285@gmail.com>
2026-04-20 15:44:24 +0000

    ignore updated

--- templates/single-instance/keypair.tf
+++ templates/single-instance/keypair.tf
@@ -13,6 +13,6 @@ resource "openstack_compute_keypair_v2" "terraform_key" {
 # 3. Anahtarın SSH atmanı sağlayacak (.pem) dosyasını klasörüne indirir
 resource "local_file" "private_key_save" {
   content         = tls_private_key.vmind_key.private_key_pem
-  filename        = "vmind-anahtar.pem" # Dosya adın bu olacak
+  filename        = "vmind-terraform-anahtar.pem" # Dosya adın bu olacak
   file_permission = "0600"
 }
