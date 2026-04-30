397ce181fc37e3b129044de6545b9c6e9802ec71
Umut Can <118760554+umutxcan@users.noreply.github.com>
2026-04-30 15:27:22 +0300

    Remove comment from providers.tf

--- single-instance/providers.tf
+++ single-instance/providers.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     openstack = {
-      # MOST CRITICAL POINT:
+      
       source  = "terraform-provider-openstack/openstack"
       version = "~> 1.53.0"
     }
