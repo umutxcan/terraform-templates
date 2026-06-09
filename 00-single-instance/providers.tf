cc8bb0fe508ae4fd6f3a66de44b07f4d40008b56
Umut Can <118760554+umutxcan@users.noreply.github.com>
2026-06-09 14:11:03 +0300

    refactor: update providers.tf to use portvmind_username and portvmind_password variables

--- 00-single-instance/providers.tf
+++ 00-single-instance/providers.tf
@@ -10,8 +10,8 @@ terraform {
 
 provider "openstack" {
   auth_url    = "https://tr-ist-01-apigw.portvmind.com/v3"
-  user_name   = var.vmind_user
-  password    = var.vmind_pass
+  user_name   = var.portvmind_username
+  password    = var.portvmind_password
   tenant_id   = var.vmind_tenant_id 
   domain_name = "Default"
   region      = "tr-ist-01"
